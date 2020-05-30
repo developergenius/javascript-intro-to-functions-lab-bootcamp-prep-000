10f7bd996f3dbcd0c2b5d2c9457b5038b7aa349f
Damion Stewart <developer_genius@pm.me>
2020-05-30 10:47:52 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var lowercase = 'hello'
+  var uppercase = 'HELLO'
+  var text = "I love you, Grandma."
+  
+  if(lowercase.toLowerCase() === lowercase){
+    return "I can't hear you!"
+  }
+  else if(uppercase.toUpperCase() === uppercase){
+    return "YES INDEED!"
+  }
+  else(text.toLowerCase && text.toUpperCase === text) 
+    return "I love you, too."
+  
+}
